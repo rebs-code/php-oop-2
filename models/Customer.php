528dab71d413a4fe46013794c76c088a749de00a
rebs-code <jack.rebussi@gmail.com>
2024-01-16 19:19:41 +0100

    add: credit card property and method to check if customer has a valid credit card

--- models/Customer.php
+++ models/Customer.php
@@ -8,6 +8,7 @@ class Customer
     public $name;
     private $isRegistered;
     private $discount;
+    private $creditCard;
 
     /**
      * __construct
@@ -16,11 +17,17 @@ class Customer
      * @param  mixed $isRegistered
      * @return void
      */
-    public function __construct($name, $isRegistered = false)
+    public function __construct($name, $isRegistered = false, CreditCard $creditCard = null)
     {
         $this->name = $name;
         $this->isRegistered = $isRegistered;
+        $this->creditCard = $creditCard;
         $this->calculateDiscount();
+    }
+
+    public function hasValidCreditCard()
+    {
+        return $this->creditCard && $this->creditCard->isValid();
     }
 
     private function calculateDiscount()
