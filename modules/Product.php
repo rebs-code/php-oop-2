994bb7945f2fab9a01751fab2112614897712da7
rebs-code <jack.rebussi@gmail.com>
2024-01-16 17:24:31 +0100

    add: type property to Product class constructor

--- modules/Product.php
+++ modules/Product.php
@@ -6,12 +6,14 @@ class Product
     public $price;
     public $description;
     public $category;
+    public $type;
 
-    public function __construct($name, $price, $description, Category $category)
+    public function __construct($name, $price, $description, Category $category, $type)
     {
         $this->name = $name;
         $this->price = $price;
         $this->description = $description;
         $this->category = $category;
+        $this->type = $type;
     }
 }
