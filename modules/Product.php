a6be73702f8a65eab6e24ff7d2d4482c15504a68
rebs-code <jack.rebussi@gmail.com>
2024-01-16 16:36:26 +0100

    feat: Product.php class

---
+++ modules/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+class Product
+{
+    public $name;
+    public $price;
+    public $description;
+    public $category;
+
+    public function __construct($name, $price, $description, $category)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->description = $description;
+        $this->category = $category;
+    }
+}
